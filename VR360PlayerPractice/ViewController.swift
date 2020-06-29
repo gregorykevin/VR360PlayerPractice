3b8f5580083d61230639654546e16d7944d4b938
Gregory Kevin <gregorykevin1211@gmail.com>
2020-06-29 16:26:55 +0700

    Initial Commit

---
+++ VR360PlayerPractice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VR360PlayerPractice
+//
+//  Created by Gregory Kevin on 29/06/20.
+//  Copyright © 2020 Gregory. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
